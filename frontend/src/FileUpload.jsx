b20b7fe72cb086d2f8ebbbf28a5d42cab416528c
ansonkwokys <ansonyskwok@gmail.com>
2025-03-22 18:23:06 -0400

    feat: add fetching for document uploading

--- frontend/src/FileUpload.jsx
+++ frontend/src/FileUpload.jsx
@@ -2,19 +2,29 @@ import { useState, useEffect } from "react";
 
 export default function FileUpload() {
     const [file, setFile] = useState(null);
+    const [isButtonClicked, setIsButtonClicked] = useState(0);
 
-	useEffect(() => {
-		if (file != null){
-			fetch("http://localhost:3000/", 
-				{
-					method: "POST",
-					body: JSON.stringify({file: file})
-				}
-			)
-            .then((response) => console.log(response))
-            .catch((error) => console.error(error));
-		}
-	}, [file])
+    useEffect(() => {
+        if (isButtonClicked) {
+            if (file == null) {
+                alert("file not uploaded!");
+            } else {
+				const formData = new FormData();
+                formData.append('file', file);
+                fetch("http://0.0.0.0:8000/getdatafromfile", {
+                    method: "POST",
+                    body: formData,
+                })
+                    .then((response) => 
+						response.json()
+						)
+					.then((data) => {
+						console.log(data)
+					})
+                    .catch((error) => console.error(error));
+            }
+        }
+    }, [isButtonClicked, file]);
 
     // Handle file selection
     const handleFileChange = (event) => {
@@ -30,9 +40,9 @@ export default function FileUpload() {
     // Handle file upload (example: console logging)
     const handleUpload = () => {
         if (file) {
+            setIsButtonClicked(1);
             console.log("File uploaded:", file);
             alert(`Uploaded: ${file.name}`);
-			
         } else {
             alert("No file selected!");
         }
