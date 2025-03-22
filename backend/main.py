6714cf40fbc54e5ba052c4821fff5899552018e0
Allan Su <67175442+tudourocky@users.noreply.github.com>
2025-03-22 01:19:43 -0400

    setup mongoDB and updated requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,22 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://admin:adminpassword@moneytreecluster.b7nzf.mongodb.net/?retryWrites=true&w=majority&appName=MoneyTreeCluster"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
