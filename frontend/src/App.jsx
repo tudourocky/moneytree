eb8225d2fc15f308693293b53720d511bea1a5b6
Allan Su <67175442+tudourocky@users.noreply.github.com>
2025-03-22 21:32:49 -0400

    verified all backend requests

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import FileUpload from "./FileUpload.jsx"
 
@@ -10,35 +9,3 @@ function App() {
 
 
 export default App;
-
-  // Handle file upload (example: console logging)
-  const handleUpload = () => {
-    if (file) {
-      console.log("File uploaded:", file);
-      alert(`Uploaded: ${file.name}`);
-    } else {
-      alert("No file selected!");
-    }
-  };
-
-  return (
-    
-    <div className="file-upload-box">
-      <div className="vertical-line"></div>
-      <label className="file-input-label">
-        <input type="file" accept="application/pdf" onChange={handleFileChange} />
-        <div className="upload-box">📄 Click or Drag to Upload PDF</div>
-      </label>
-      
-      {file && <p>Selected File: {file.name}</p>}
-
-      <button onClick={handleUpload} className="upload-button">
-        Upload
-      </button>
-    </div>
-  );
-};
-
-
-
-export default FileUpload;
